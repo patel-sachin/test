116b85c996eecfd86abf93a9d2acf8c5a7e9cae8
patel-sachin <sachin.patel.n@gmail.com>
2024-02-24 13:43:41 -0600

    Add files via upload

---
+++ .zshrc
@@ -0,0 +1,2 @@
+# nix package manager (required to run nix-env command)
+if [ -e /home/spatel/.nix-profile/etc/profile.d/nix.sh ]; then . /home/spatel/.nix-profile/etc/profile.d/nix.sh; fi
---
+++ install.sh
@@ -0,0 +1,51 @@
+#-----------------------------------------------------
+# install curl
+#-----------------------------------------------------
+sudo apt install curl
+
+#-----------------------------------------------------
+# install nix package manager
+#-----------------------------------------------------
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+
+#-----------------------------------------------------
+# source nix package manager
+#-----------------------------------------------------
+. ~/.nix-profile/etc/profile.d/nix.sh
+
+#-----------------------------------------------------
+# install packages from nix packages
+#-----------------------------------------------------
+nix-env -iA \ 
+        nixpkgs.zsh \
+        nixpkgs.antidote \
+        nixpkgs.git \
+        nixpkgs.neovim \
+        nixpkgs.tmux \
+        nixpkgs.stow \
+        nixpkgs.nvm \
+        nixpkgs.fzf \
+        nixpkgs.ripgrep \
+        nixpkgs.bat 
+
+#-----------------------------------------------------
+# add zsh to the list of available shells
+#-----------------------------------------------------
+command -v zsh | sudo tee -a /etc/shells
+
+#-----------------------------------------------------
+# make zsh the default shell
+#-----------------------------------------------------
+sudo chsh -s $(which zsh) $USER
+
+#-----------------------------------------------------
+# Once the terminal is opened, the default shell will
+# be zsh and this entry needs to be there in the 
+# .zshrc for the nix-env command to work. This has 
+# already been added to the .zshrc file that is committed
+# in git.
+#
+# if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then . ~/.nix-profile/etc/profile.d/nix.sh; fi
+#-----------------------------------------------------
+
+
