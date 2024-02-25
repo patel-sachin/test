12f9986129b7c70687d773bd3f3ffefde76a53cc
patel-sachin <sachin.patel.n@gmail.com>
2024-02-25 17:07:55 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,16 +38,6 @@ command -v zsh | sudo tee -a /etc/shells
 #-----------------------------------------------------
 sudo chsh -s $(which zsh) $USER
 
-#-----------------------------------------------------
-# Once the terminal is opened, the default shell will
-# be zsh and this entry needs to be there in the 
-# .zshrc for the nix-env command to work. This has 
-# already been added to the .zshrc file that is committed
-# in git.
-#
-# if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then . ~/.nix-profile/etc/profile.d/nix.sh; fi
-#-----------------------------------------------------
-
 #
 # bundle zsh plugins
 #
