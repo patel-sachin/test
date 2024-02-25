a396b5ff5c27faad4b4cd005ae8a0aded067d8f5
patel-sachin <sachin.patel.n@gmail.com>
2024-02-25 00:28:32 -0600

    Create plugins.sh

---
+++ plugins.sh
@@ -0,0 +1,11 @@
+nix-env -iA \ 
+        nixpkgs.zsh \
+        nixpkgs.antidote \
+        nixpkgs.git \
+        nixpkgs.neovim \
+        nixpkgs.tmux \
+        nixpkgs.stow \
+        nixpkgs.fzf \
+        nixpkgs.ripgrep \
+        nixpkgs.bat \
+        nixpkgs.direvn
